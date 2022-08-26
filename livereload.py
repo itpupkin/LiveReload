fd83d016ef9f2852cfd5ebda7d080cda46fa9b92
Dmitrii Kondrashov <kdm060908@gmail.com>
2022-08-26 16:00:42 +0600

    Add Files

---
+++ livereload.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from filehash import FileHash as fh
+import time
+import json
+
+with open("config.json") as cfg:
+    config = json.load(cfg)
+
+filepath = config["filepath"]
+browserpath = config["browserpath"]
+
+md5hasher = fh('md5')
+filehash = md5hasher.hash_file(filepath)
+
+driver = webdriver.Chrome()
+
+driver.get(browserpath)
+
+while True:
+    if filehash != md5hasher.hash_file(filepath):
+        filehash = md5hasher.hash_file(filepath)
+        driver.refresh()
+        pass
+
+    time.sleep(0.3)
